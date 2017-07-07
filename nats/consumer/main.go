94d43db6e94dd9fbc6bd3a97e0c07d93ce424cdd
Dave Pederson <dave.pederson@gmail.com>
2017-07-06 22:28:18 -0600

    Add NATS example

---
+++ nats/consumer/main.go
@@ -0,0 +1,54 @@
+// Copyright 2017 Dave Pederson.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"github.com/zdavep/dozer"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+)
+
+// Example message handler function.
+func messageHandler(id int, messages chan []byte) {
+	for message := range messages {
+		log.Printf("%d: Received [ %s ]\n", id, string(message))
+	}
+}
+
+// Consume messages from a Kafka topic.
+func main() {
+
+	// Create a dozer Kafka consumer instance
+	dz := dozer.Init("test").WithProtocol("nats").Consumer()
+	err := dz.Dial("localhost", 4222)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Helper channels
+	messages, quit := make(chan []byte), make(chan bool)
+
+	// Dedicate some go-routines for message processing.
+	for i := 1; i <= runtime.NumCPU()/2+1; i++ {
+		go messageHandler(i, messages)
+	}
+
+	// Start receiving messages
+	log.Println("Receiving messages; hit [ctrl-c] to quit.")
+	go func() {
+		if err := dz.RecvLoop(messages, quit); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Listen for [ctrl-c] interrupt signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	<-interrupt
+	signal.Stop(interrupt)
+	quit <- true
+	os.Exit(0)
+}
